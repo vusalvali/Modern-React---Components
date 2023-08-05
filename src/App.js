e60cb8a2886509bcd452e85319f4a960fd1c3338
Vusal Vali <velivusal@gmail.com>
2023-08-05 21:39:17 +0400

    Refactoring done adding Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const items = [
+        {
+            id: 'asasq',
+            label:'Can I use React on a project?',
+            content: 'You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.You can use React on any project you want. '
+        },
+        {
+            id: 'as24wdasq',
+            label: 'Can I use JavaScript on a project?',
+            content:'You can use Javascript on any project you want.You can use Javascript on any project you want.You can use Javascript on any project you want.You can use Javascript on any project you want.'
+        },
+        {
+            id: 'ad32eqwsasq',
+            label:'Can I use CSS on a project?',
+            content: 'You can use CSS on any project you want.You can use CSS on any project you want.You can use CSS on any project you want.You can use CSS on any project you want.'
+        }
+    ]
+  return <Accordion items={items} />
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,42 @@
+import { GoBell, GoDownload, GoDatabase } from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("salam");
+  };
+  return (
+    <div>
+      <div>
+        <Button secondary outline rounded className='mb-5' onClick={handleClick}>
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
